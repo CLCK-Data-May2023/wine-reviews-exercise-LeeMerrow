84a5463ecbf0c65d64aa230aa5cd6d47a0dc3f17
Lee Merrow <leemerrow@gmail.com>
2023-08-19 12:34:24 -0400

    added reviews.py and reviews-per-country.csv

--- reviews.py
+++ reviews.py
@@ -1,2 +1,13 @@
 # add your code here
+import pandas as pd
 
+reviews = pd.read_csv('data\winemag-data-130k-v2.csv.zip', index_col=0)
+
+reviews_per_country = reviews.country.value_counts()
+
+reviews_points = reviews.groupby('country')['points'].mean().round(1)
+
+reviews_merge = pd.DataFrame.merge(reviews_per_country, reviews_points, on='country', how='inner')
+
+with open('data\\reviews-per-country.csv', 'w') as csv_file:
+    reviews_merge.to_csv(path_or_buf=csv_file)
